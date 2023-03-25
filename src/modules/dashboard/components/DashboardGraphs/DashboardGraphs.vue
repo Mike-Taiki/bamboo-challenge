7c56da74469a1895be3664214521a1e0122141b4
Michael Gomes Ferreira <michael.21.2012@hotmail.com>
2023-03-25 05:38:25 -0300

    add over/under graph

--- src/modules/dashboard/components/DashboardGraphs/DashboardGraphs.vue
+++ src/modules/dashboard/components/DashboardGraphs/DashboardGraphs.vue
@@ -1,5 +1,6 @@
 <template>
   <div v-if="Object.keys(chartData).length">
+    <UnderOverChart :chartData="chartData" />
     <PieChart :chartData="chartData" />
     <PddTable :chartData="chartData" />
   </div>
@@ -7,11 +8,13 @@
 <script>
 import PddTable from "../PddTable/PddTable.vue";
 import PieChart from "../PieChart/PieChart.vue";
+import UnderOverChart from "../UnderOverChart/UnderOverChart.vue";
 export default {
   name: "DashboardGraphs",
   components: {
     PddTable,
     PieChart,
+    UnderOverChart,
   },
   props: {
     chartData: {
